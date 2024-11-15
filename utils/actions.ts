7e37c86238cc042cf557f9498438010ee094f49e
Denis <brokenhandq3@yandex.ru>
2024-11-15 11:24:44 +0300

    standart way to vealidate

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { redirect } from "next/navigation";
 import db from "../utils/db";
+import { currentUser } from "@clerk/nextjs/server";
 
 // IT IS MORE EXPLICIT
 export const fetchFeaturedProducts = async () => {
@@ -47,12 +48,69 @@ export const fetchProduct = async (productId: string) => {
   return product;
 };
 
-export const createProductAction = async (
+export const createProductAction__ = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
   const answer = { message: "product created from SCR" };
+  const name = formData.get("name");
+  const price = formData.get("price");
+  const image = formData.get("image");
+  const description = formData.get("description");
   console.log("answer", answer);
   await new Promise((resolve) => setTimeout(resolve, 15000));
   return answer;
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  return { message: error instanceof Error ? error.message : "there was an error" };
+};
+
+export const createProductAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  // const answer = { message: "product created from SCR" };
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  // TS ERROR
+  const user = await getAuthUser();
+
+  try {
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    const price = Number(formData.get("price") as string);
+    //temp
+    const image = formData.get("image") as File;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: "../public/img1.jpeg",
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+
+    return { message: "product created" };
+  } catch (error) {
+    console.log(error);
+    // hard code error unknown
+    return renderError(error);
+    // return { message: error instanceof Error ? error.message : "there was an error" };
+  }
+  // console.log("answer", answer, { name, price, image });
+  // return answer;
 };
